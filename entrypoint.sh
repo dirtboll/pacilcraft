c85360f20ce1cc4515b2be2b11cf62c63f84a76d
dirtboll <dirtboll.boi@gmail.com>
2022-10-23 18:06:42 +0000

    Update to 1.19.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -euxo pipefail
+
 cp -pR /extras/overrides/** /data
 sudo chown -R 1000:1000 /data
+sudo find /data/plugins/ -type f -name "*.jar" -delete
+sudo find /data/plugins/ -type f -name "*-version.json" -delete
 /start
